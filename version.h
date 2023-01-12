ce3a0523661c412c033a5025e4c5acc57068e027
D.A.Tiger <drakarax@seznam.cz>
2023-01-12 19:57:44 +0100

    Change Gorgona version

--- version.h
+++ version.h
@@ -2,12 +2,11 @@
 #define VERSION_H
 
 namespace AutoVersion{
-	
 	//Date Version Types
-	static const char DATE[] = "28";
-	static const char MONTH[] = "03";
-	static const char YEAR[] = "2019";
-	static const char UBUNTU_VERSION_STYLE[] =  "19.03";
+	static const char DATE[] = "11";
+	static const char MONTH[] = "01";
+	static const char YEAR[] = "2023";
+	static const char UBUNTU_VERSION_STYLE[] =  "11.01";
 	
 	//Software Status
 	static const char STATUS[] =  "Alpha";
@@ -15,19 +14,17 @@ namespace AutoVersion{
 	
 	//Standard Version Type
 	static const long MAJOR  = 1;
-	static const long MINOR  = 0;
+	static const long MINOR  = 5;
 	static const long BUILD  = 0;
-	static const long REVISION  = 0;
+	static const long REVISION  = 1;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT  = 5;
-	#define RC_FILEVERSION 1,0,0,0
-	#define RC_FILEVERSION_STRING "1, 0, 0, 0\0"
-	static const char FULLVERSION_STRING [] = "1.0.0.0";
+	#define RC_FILEVERSION 1,5,0,1
+	#define RC_FILEVERSION_STRING "1, 5, 0, 1\0"
+	static const char FULLVERSION_STRING [] = "1.5.0.1";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY  = 0;
-	
-
 }
 #endif //VERSION_H
