0dae3623c0403206558701affda7cbdf5d5a82e2
D.A.Tiger <drakarax@seznam.cz>
2023-04-01 11:15:54 +0200

    Adding shared headers

--- include/define.h
+++ include/define.h
@@ -22,12 +22,29 @@
 * along with this program.  If not, see <https://www.gnu.org/licenses/>. *
 *************************************************************************/
 
+// The standard routine & objects, syscalls and system types
+#include <cstdio>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
+#include <iostream>
+
+// The LUA interpreter
+#include<lua5.1/lualib.h>
+#include<lua5.1/lauxlib.h>
+
+// Tiny xml - xml support
+#include<tinyxml.h>
+//#include<tinyxml2.h>
+
+// Foxlib & FoxGHI - GUI libraries
 #include<fox-1.7/fx.h>
 #include<fxghi.h>
-#include<iostream>
-#include<sys/wait.h>
-#include<tinyxml2.h>
 
+// Internal shared header files
+#include<IconsTheme.h>
 #include<Utils.h>
 
 //#define __DEBUG
