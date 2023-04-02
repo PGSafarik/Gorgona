c14f758205299c1d5d660225d7243371c777c0b2
D.A.Tiger <drakarax@seznam.cz>
2023-04-02 23:43:14 +0200

    Increment DEVEL version

--- version.h
+++ version.h
@@ -3,26 +3,26 @@
 
 namespace AutoVersion{
 	//Date Version Types
-	static const char DATE[] = "19";
-	static const char MONTH[] = "03";
-	static const char YEAR[] = "2023";
+	static const char DATE[]                 = "19";
+	static const char MONTH[]                = "03";
+	static const char YEAR[]                 = "2023";
 	static const char UBUNTU_VERSION_STYLE[] =  "19.03";
 	
 	//Software Status
-	static const char STATUS[] =  "Alpha";
-	static const char STATUS_SHORT[] =  "a";
+	static const char STATUS[]     = "Devel"; // Alpha, Beta, Devel, Testing, Stable
+	static const char STATUS_SHORT = 'D';     // A, B, D, T, S
 	
 	//Standard Version Type
-	static const long MAJOR  = 1;
-	static const long MINOR  = 6;
-	static const long BUILD  = 0;
-	static const long REVISION  = 0;
+	static const long MAJOR    = 1;
+	static const long MINOR    = 6;
+	static const long BUILD    = 3;
+	static const long REVISION = 1;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT  = 1;
-	#define RC_FILEVERSION 1,6,0,0
-	#define RC_FILEVERSION_STRING "1, 6, 0, 0\0"
-	static const char FULLVERSION_STRING [] = "1.6.0.0";
+	#define RC_FILEVERSION 1,6,3,1
+	#define RC_FILEVERSION_STRING "1, 6, 3, 1\0"
+	static const char FULLVERSION_STRING [] = "1.6.3.1";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY  = 0;
