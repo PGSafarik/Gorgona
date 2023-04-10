cf60cc03f948819a2eb30a7b7772c252b1544a82
D.A.Tiger <drakarax@seznam.cz>
2023-04-11 00:53:16 +0200

    Change the Devel version number

--- version.h
+++ version.h
@@ -3,10 +3,10 @@
 
 namespace AutoVersion{
 	//Date Version Types
-	static const char DATE[]                 = "19";
-	static const char MONTH[]                = "03";
+	static const char DATE[]                 = "11";
+	static const char MONTH[]                = "04";
 	static const char YEAR[]                 = "2023";
-	static const char UBUNTU_VERSION_STYLE[] =  "19.03";
+	static const char UBUNTU_VERSION_STYLE[] =  "11.04";
 	
 	//Software Status
 	static const char STATUS[]     = "Devel"; // Alpha, Beta, Devel, Testing, Stable
@@ -15,14 +15,14 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR    = 1;
 	static const long MINOR    = 6;
-	static const long BUILD    = 3;
+	static const long BUILD    = 4;
 	static const long REVISION = 1;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT  = 1;
-	#define RC_FILEVERSION 1,6,3,1
-	#define RC_FILEVERSION_STRING "1, 6, 3, 1\0"
-	static const char FULLVERSION_STRING [] = "1.6.3.1";
+	#define RC_FILEVERSION 1,6,4,1
+	#define RC_FILEVERSION_STRING "1, 6, 4, 1\0"
+	static const char FULLVERSION_STRING [] = "1.6.4.1";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY  = 0;
