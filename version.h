ba258936edfa92bc2d7c41c4b4b5fd3af950895f
D.A.Tiger <drakarax@seznam.cz>
2023-08-03 11:15:31 +0200

    Change version on: 1.6.4.3 03.08 D

--- version.h
+++ version.h
@@ -3,10 +3,10 @@
 
 namespace AutoVersion{
 	//Date Version Types
-	static const char DATE[]                 = "23";
-	static const char MONTH[]                = "05";
+	static const char DATE[]                 = "03";
+	static const char MONTH[]                = "08";
 	static const char YEAR[]                 = "2023";
-	static const char UBUNTU_VERSION_STYLE[] =  "23.05";
+	static const char UBUNTU_VERSION_STYLE[] =  "03.08";
 	
 	//Software Status
 	static const char STATUS[]     = "Devel"; // Alpha, Beta, Devel, Testing, Stable
@@ -16,13 +16,13 @@ namespace AutoVersion{
 	static const long MAJOR    = 1;
 	static const long MINOR    = 6;
 	static const long BUILD    = 4;
-	static const long REVISION = 2;
+	static const long REVISION = 3;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT  = 1;
-	#define RC_FILEVERSION 1,6,4,2
-	#define RC_FILEVERSION_STRING "1, 6, 4, 2\0"
-	static const char FULLVERSION_STRING [] = "1.6.4.2";
+	#define RC_FILEVERSION 1,6,4,3
+	#define RC_FILEVERSION_STRING "1, 6, 4, 3\0"
+	static const char FULLVERSION_STRING [] = "1.6.4.3";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY  = 0;
