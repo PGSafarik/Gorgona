8cf5d128fe4ea4fe6b7df3d0d74316e4b43590a2
D.A.Tiger <drakarax@seznam.cz>
2023-11-13 22:00:26 +0100

    Add ECHIDNA::XMLElementStream<T> interface

---
+++ include/Echidna/XMLUtils.h
@@ -0,0 +1,35 @@
+/*************************************************************************
+* This program is free software: you can redistribute it and/or modify   *
+* it under the terms of the GNU General Public License as published by   *
+* the Free Software Foundation, either version 3 of the License, or      *
+* (at your option) any later version.                                    *
+*                                                                        *
+* This program is distributed in the hope that it will be useful,        *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+* GNU General Public License for more details.                           *
+*                                                                        *
+* You should have received a copy of the GNU General Public License      *
+* along with this program.  If not, see <https://www.gnu.org/licenses/>. *
+*************************************************************************/
+#include<define.h>
+
+#ifndef __XMLUTILS_H
+#define __XMLUTILS_H
+
+/*************************************************************************
+* XMLUtils.h                                                             *
+*                                                                        *
+* Small objects for manipulation with Tiny XML                           *
+* FXObjectListOf and FXObjectList                                        *
+* Copyright (c) 05/05/2018 D.A.Tiger <drakarax@seznam.cz>                *
+*************************************************************************/
+
+/* Policy xml stream element */
+template<class T> struct XMLElementStream : public T 
+{
+  void operator <<( const XMLElement &source ) { T::read( source ); }
+  void operator >>( XMLElement &target )       { T::write( target ); } 
+};
+
+#endif /* __XMLUTILS_H */
