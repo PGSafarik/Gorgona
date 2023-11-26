e94130514663989231b477a16dc50b8f52a98555
D.A.Tiger <drakarax@seznam.cz>
2023-11-26 15:01:20 +0100

    Insert the ItemBase class of the ECHIDNA namespace

---
+++ include/Echidna/ItemBase.h
@@ -0,0 +1,106 @@
+/*************************************************************************
+* This program is free software: you can redistribute it and/or modify   *
+* it under the terms of the GNU General Public License as published by   *
+* the Free Software Foundation, either version 3 of the License, or      *
+* (at your option) any later version.                                    *
+*                                                                        *
+* This program is distributed in the hope that it will be useful,        *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+* GNU General Public License for more details.                           *
+*                                                                        *
+* You should have received a copy of the GNU General Public License      *
+* along with this program.  If not, see <https://www.gnu.org/licenses/>. *
+*************************************************************************/
+//#include<define.h>
+
+#ifndef __ITEMBASE_H
+#define __ITEMBASE_H
+/*************************************************************************
+* ItemBase.h                                                             *
+*                                                                        *
+* Small class template replacing the non-conforming classes              *
+* FXObjectListOf and FXObjectList                                        *
+* Copyright (c) 05/05/2018 D.A.Tiger <drakarax@seznam.cz>                *
+*************************************************************************/
+namespace ECHIDNA {
+
+//class Gorgona;
+
+enum {
+  SEL_CHANGE_INCR = SEL_LAST,
+  SEL_CHANGE_DECR,
+  SEL_LAST
+};
+
+
+class ItemBase : public FXObject {
+FXDECLARE( ItemBase )
+  // <TYPE param1="data" param="data" />
+  Gorgona            *m_app;      // Ukazatel na instanci aplikace 
+  FXString            m_type;      // Typ polozky   ( "Game", "tool", "label" )  
+  FXStringDictionary  m_property;  // data polozky ve stylu: pozice -> klic = hodnota
+  
+  /* Control State automation */
+  FXuint    m_csa_state;           // Cele kladne cislo, reprezentujici aktualni stav polozky  
+  FXuint    m_csa_trans[ 5 ][ 5 ]; // Seznam moznych prechodu automatu
+  FXuint    m_csa_ends[ 2 ];       // Seznam koncovych stavu
+  FXbool    m_csa_sleep;           // Pozastaveni automatu v urcitem stavu - POZOR velmi zakarne!
+  void   SleepStateMat( FXbool s ) { m_csa_sleep = s; } /* Pozastavi stavovy automat - POZOR: VELMI NEBEZPECNE A ZRADNE, opravdu osklivy workarround */
+  
+  /* notifications */
+  FXObject   *m_target;
+  FXSelector  m_msg;
+  
+public :
+  ItemBase( Gorgona *a, const FXString &type = "UNKNOWN" );    
+  virtual ~ItemBase( );
+  
+  /*** CSA States flags ***/
+  enum {
+    CSA_STATE_READY = 0, // Normalni stav ( prvni koncovy stav )
+    CSA_STATE_EMPTY,     // Polozka je prazdna (vychozi stav a druhy koncovy stav) 
+    CSA_STATE_MODIFY,    // Vnitrni hodnoty polozky byly zmeneny
+    CSA_STATE_ACTIVE,    // Polozka je aktivni (napr. bezici proces hry)
+    CSA_STATE_REMOVED,   // Polozka bude odstranena
+    CSA_STATE_LAST,      // Info o  Item State automation viz. slozka tmp/Diagrams
+  };
+
+  /*** Access methods ***/
+  Gorgona*   get_app( ) { return m_app; }
+  FXuint     get_state( ) { return m_csa_state; }
+  FXbool     set_property( const FXString &key, const FXString &value ); 
+  FXString   get_property( const FXString &name );
+  FXObject*  get_target( ) { return m_target; }
+  void       set_object( FXObject *tgt ) { m_target = tgt; };
+  FXSelector get_message( ) { return m_msg; }  
+  void       set_message( FXSelector msg ) { m_msg = msg; }
+  const FXString get_type( ) const { return m_type; }
+  
+  /* Operations */
+  FXint  load( XMLElement *store );
+  FXint  save( XMLElement *store );
+  FXbool clear( FXbool force = false );
+
+protected: 
+  ItemBase( ) { }
+ 
+  /* internal Control State Automaton methods */
+  void InitStateMat( );
+  /* Toto je kriticka funkce interniho stavoveho automatu Provadi kontrolu zda je mozno
+   * v dane situaci prejit do jineho konkretniho stavu.  
+   * notify = 0 - zakaza notifikazi uspesneho prechodu.  */
+  FXbool CheckState( FXuint svalue, FXuint notify = SEL_CHANGED, FXbool enable_same = false ); 
+  
+  /*** internal I/O methods ***/
+  virtual FXint _Save( XMLElement  *my_element ) { return 0; }
+  virtual FXint _Load( XMLElement  *my_element ) { return 0; }
+  virtual FXint _Erase( XMLElement *my_element ) { return 0; }  
+  FXbool  SetProperty( const FXString &key, const FXString &value );
+  long    SendNotify( FXuint msg_type, void *msg_data = NULL );
+};      /* class ItemBase    */
+
+};      /* namespace ECHIDNA */
+#endif  /* __ITEMBASE_H      */
+
+/*** END ******************************************************************************************/
---
+++ src/Echidna/ItemBase.cpp
@@ -0,0 +1,194 @@
+// ItemBase.cpp Copyright (c) %date%;  D.A.Tiger; GNU GPL 3
+#include<Gorgona.h>
+#include<Echidna/ItemBase.h>
+
+using namespace ECHIDNA;
+namespace ECHIDNA {
+
+FXIMPLEMENT( ItemBase, FXObject, NULL, 0 )
+
+ItemBase::ItemBase( Gorgona *a, const FXString &type ) 
+{
+  m_app    = a;
+  m_type   = type;
+
+  InitStateMat( );
+}
+
+ItemBase::~ItemBase( )
+{ }
+
+/**************************************************************************************************/
+FXbool ItemBase::set_property( const FXString &key, const FXString &value )
+{
+  if( !key.empty( ) && !value.empty( ) ) { 
+    if( CheckState( ItemBase::CSA_STATE_MODIFY, SEL_CHANGE_INCR ) ) { return SetProperty( key, value ); }    
+  }
+
+  return false;
+}
+
+FXString ItemBase::get_property( const FXString &key )
+{
+  FXString v = FXString::null;
+  
+  if( key.empty( ) ) { v = m_type; }
+  else {
+    FXint pos = m_property.find( key );
+    if( pos != -1 ) { v = m_property.data( pos ); } 
+  }
+  
+  return v;
+}
+
+/**************************************************************************************************/
+FXint ItemBase::load( XMLElement *store )
+{
+  FXint res = -1;
+  
+  if( store && CheckState( ItemBase::CSA_STATE_READY, SEL_CHANGE_DECR ) ) {
+    XMLElement *record = store->FirstChildElement( m_type.text( ) );
+    
+    if( record ) {
+      const XMLAttribute *attr = record->FirstAttribute( );
+      while( attr != NULL ) {
+        SetProperty( attr->Name( ), attr->Value( ) ); 
+        attr = attr->Next( );
+      }
+      res = _Load( record );
+    }
+  }
+  
+  return res;
+}
+
+FXint ItemBase::save( XMLElement *store ) 
+{
+  FXint       res = -1;
+  FXuint      state = get_state( );
+  XMLElement *record = store->FirstChildElement( m_type.text( ) );
+  
+  switch( state ) {
+    case ItemBase::CSA_STATE_MODIFY : {
+      if( CheckState( ItemBase::CSA_STATE_READY, SEL_CHANGE_DECR ) ) {
+        if( !record ) { record = store->InsertNewChildElement( m_type.text( ) ); }
+        FXint p_num = m_property.no( );
+        for( FXint i = 0; i != p_num; i++ ) {
+          if( !m_property.empty( i ) ) {
+            FXString p_key = m_property.key( i );
+            FXString p_val = m_property.data( i );
+            std::cout << m_type << "[" << p_key << "] = " << p_val << std::endl;
+            if( !p_val.empty( ) ) { record->SetAttribute( p_key.text( ), p_val.text( ) ); }
+          }
+        }
+        
+        res = _Save( record );
+      }  
+      break; 
+    }
+    case ItemBase::CSA_STATE_REMOVED : {
+      if( CheckState( ItemBase::CSA_STATE_EMPTY, SEL_CHANGED ) ) {
+        clear( );
+        if( record ) {
+          _Erase( record );
+          store->DeleteChild( record );
+        }
+      }  
+      break; 
+    }
+    default: { res = 0; }
+  }  
+
+  return res;
+}
+
+FXbool ItemBase::clear( FXbool force )
+{
+  FXbool res = false;
+  FXuint next_state = ( force ? ItemBase::CSA_STATE_REMOVED : ItemBase::CSA_STATE_MODIFY );
+
+  // Kvuli kompilatoru: warning: enumerated mismatch in conditional expression: 'FX::<unnamed enum>' vs 'ECHIDNA::<unnamed enum>' [-Wenum-compare]
+	FXuint msg =  SEL_CHANGED; 
+  if( force ) { msg = SEL_CHANGE_INCR; }      
+  
+  if( CheckState( next_state, msg ) ) {
+    m_property.clear( );
+    res = true;
+  }
+  
+  return res;
+}
+
+/**************************************************************************************************/
+void ItemBase::InitStateMat( )
+{  
+  for( FXuint i = 0; i != 5; i++ ) {
+    for( FXuint j = 0; j != 5; j++ ) {
+       m_csa_trans[ i ][ j ] = 0;
+    }
+  }  
+  
+  m_csa_trans[ ItemBase::CSA_STATE_READY   ][ ItemBase::CSA_STATE_MODIFY  ] = 1;
+  m_csa_trans[ ItemBase::CSA_STATE_READY   ][ ItemBase::CSA_STATE_ACTIVE  ] = 1;
+  m_csa_trans[ ItemBase::CSA_STATE_READY   ][ ItemBase::CSA_STATE_REMOVED ] = 1;
+  m_csa_trans[ ItemBase::CSA_STATE_EMPTY   ][ ItemBase::CSA_STATE_READY   ] = 1;
+  m_csa_trans[ ItemBase::CSA_STATE_EMPTY   ][ ItemBase::CSA_STATE_MODIFY  ] = 1;
+  m_csa_trans[ ItemBase::CSA_STATE_MODIFY  ][ ItemBase::CSA_STATE_READY   ] = 1;
+  m_csa_trans[ ItemBase::CSA_STATE_MODIFY  ][ ItemBase::CSA_STATE_MODIFY  ] = 1;
+  m_csa_trans[ ItemBase::CSA_STATE_ACTIVE  ][ ItemBase::CSA_STATE_MODIFY  ] = 1;
+  m_csa_trans[ ItemBase::CSA_STATE_REMOVED ][ ItemBase::CSA_STATE_READY   ] = 1;
+  m_csa_trans[ ItemBase::CSA_STATE_REMOVED ][ ItemBase::CSA_STATE_EMPTY   ] = 1;
+  
+  m_csa_ends[ 0 ] = ItemBase::CSA_STATE_READY;
+  m_csa_ends[ 1 ] = ItemBase::CSA_STATE_EMPTY;
+  
+  m_csa_state = ItemBase::CSA_STATE_EMPTY;
+  m_csa_sleep = false;
+}
+
+FXbool ItemBase::CheckState( FXuint next_state, FXuint notify, FXbool force )
+{
+  if( m_csa_sleep ) { return true; }
+  if( m_csa_trans[ m_csa_state ][ next_state ] == 1 || force ) 
+  { 
+    FXbool differ = ( m_csa_state != next_state );
+    if( differ ) { m_csa_state = next_state; }
+   
+    if( notify > 0 && differ ) { SendNotify( notify ); }
+    
+#ifdef __DEBUG
+    std::cout << "[DEBUG - ItemBase::CheckState( ) ] " << m_type << " control automation " << ( differ ? "changed on " : " stay in " )  << " : " << m_csa_state << std::endl;
+    FXint p_num = m_property.no( );
+    for( FXint i = 0; i != p_num; i++ ) {
+      if( !m_property.empty( i ) ) {
+        FXString p_key = m_property.key( i );
+        FXString p_val = m_property.data( i );
+        std::cout << "\t" << m_type << "[" << p_key << "] = " << p_val << std::endl;
+			}
+    }
+#endif
+
+    return true; 
+  }
+ 
+  return false;      
+}
+
+/**************************************************************************************************/
+FXbool ItemBase::SetProperty( const FXString &key, const FXString &value )
+{
+  if( !key.empty( ) ) { return m_property.insert( key, value ); }
+
+  return false;
+}
+
+long ItemBase::SendNotify( FXuint msg_type, void *msg_data )
+{
+  if( m_target ) { return m_target->handle( this, FXSEL( msg_type, m_msg ), msg_data ); }
+  return 0;  
+}
+
+/**************************************************************************************************/
+}; /* namespaece ECHIDNA */
+
+/*** END ******************************************************************************************/
