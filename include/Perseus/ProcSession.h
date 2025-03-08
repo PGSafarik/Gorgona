60039983dd7db568e7fb0265c10c259eef905445
D.A.Tiger <drakarax@seznam.cz>
2025-03-08 22:03:51 +0100

    Add ProcSession class (psm)

---
+++ include/Perseus/ProcSession.h
@@ -0,0 +1,71 @@
+/*************************************************************************
+* This program is free software: you can redistribute it and/or modify   *
+* it under the terms of the GNU General Public License as published by   *
+* the Free Software Foundation, either version 3 of the License, or      *
+* (at your option) any later version.                                    *
+*                                                                        *
+* This program is distributed in the hope that it will be useful,        *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+* GNU General Public License for more details.                           *
+*                                                                        *
+* You should have received a copy of the GNU General Public License      *
+* along with this program.  If not, see <https://www.gnu.org/licenses/>. *
+*************************************************************************/
+#ifndef GORGONA_PROCESESSION_H
+#define GORGONA_PROCESESSION_H
+
+/*************************************************************************
+* ProcSession.h                                                          *
+*                                                                        *
+* Small class representing a process session                             *
+* Copyright (c) 21/01/2025 D.A.Tiger <drakarax@seznam.cz>                *
+*************************************************************************/
+#include<fcntl.h>
+
+#include<map>
+#include<string>
+#include<vector>
+
+#include<define.h>
+#include<Perseus/Process.h>
+
+//#include<boost/filesystem.hpp>
+#include<boost/asio.hpp>
+#include<boost/process.hpp>
+
+namespace BP = boost::process;
+
+namespace PERSEUS {
+  
+  class ProcSession : public std::map<FXint, PERSEUS::Process* > {
+    FXString m_name;       // Name of the Gorgona sesion
+    FXString m_id;         // ID string of the Gorgona sesion
+    pid_t    m_sid = -1;   // A identifier of then system process session
+    FXint    m_actjob = 0; // Actual set job group
+
+    std::vector<FXint>  m_maintable; // Maintable ALL descedants pid runned in this session
+    std::vector<FXint>  m_jobs;      // Table of job groups
+
+  public:
+    ProcSession( const FXString &name = "GORGONA_SESSION", const FXString &id = FXString::null );
+    virtual ~ProcSession( );
+
+    /* Access methods */
+    FXString id( )            { return m_id; } 
+    FXint    sid( )           { return static_cast<FXint>( m_sid ); }
+    FXString name( )          { return m_name; }
+    FXbool   has( FXint pid ) { auto it = find( pid ); return it != end( ); }
+    FXint    set_job( FXuint type = JOB_SYSTEM );
+ 
+    /* opeartions */
+    FXbool  start( );                               // Start to the process session
+    FXbool  terminate( FXbool fast = false);        // End to the process session     
+    FXbool  join( FXint pid, FXint group_id = 0 );  // Add pid to main table
+    FXint   check( );                               // Checking processes in Session
+
+  protected:
+  };
+  
+}      /* Namespace PERSEUS     */
+#endif /* GORGONA_PROCESESION_H */
---
+++ src/Perseus/ProcSession.cpp
@@ -0,0 +1,123 @@
+// Process.cpp Copyright (c) 21/01/2025;  D.A.Tiger; GNU GPL 3
+#include<Perseus/ProcSession.h>
+
+using namespace PERSEUS;
+namespace PERSEUS {
+
+/**************************************************************************************************/
+ProcSession::ProcSession( const FXString &name, const FXString &id ) : m_name( name ), m_id( id ) 
+{ 
+   
+}
+
+ProcSession::~ProcSession( ) 
+{ 
+  
+}
+
+/**************************************************************************************************/
+FXbool ProcSession::start( )
+{
+  if( ( m_sid = setsid( ) ) < 0 ) { 
+    std::cerr << "[ProcSession WARNING]: Failed of setsid( ) call. Is this process already the session leader?" << std::endl; 
+  }
+  
+  if( m_id.empty( ) ) { m_id = ( m_sid >= 1 ? FXString::value( m_sid ) : "1" ); }
+  //FXSystem::setEnvironment( m_name, m_id );
+
+  m_jobs.resize( ID_NORMAL_MAX + 1, 0 );
+  set_job( JOB_SYSTEM );
+  DEBUG_OUT( "[ProcSession::start] starting process session " << m_name << " (" << m_sid << ")" )
+  return true;  
+}
+
+FXbool ProcSession::terminate( FXbool fast )
+{
+  FXint check, fail;
+
+  check = fail = 0;  
+  for( auto it = this->begin( ); it != this->end( ); it++ ) {
+    PERSEUS::Process *proc = it->second;
+    if( !proc->kill( ) ) {  
+      std::cerr << "[ProcSession ERROR]: Failed of terminate process " << proc->id( ) << std::endl;
+      fail++;
+    }
+    check++;
+  } 
+  
+  if( fail > 0 ) {
+    std::cerr << "[ProcSession WARNING]: The process session " << this->id( ) << " was not terminated correctly: " << std::endl;
+    std::cerr << fail << " processes are still running!" << std::endl; 
+  }
+
+  m_sid = -1;
+  return true;  
+} 
+
+FXbool ProcSession::join( FXint pid, FXint group_id )
+{
+  if( !has( pid ) ) {
+    for( FXint n : m_maintable ) {
+      if( n == pid ) { return false; }
+    }
+    m_maintable.push_back( pid );
+    return true;
+  }  
+   
+  return false;
+}
+
+FXint ProcSession::set_job( FXuint type )
+{
+  FXint result = -1;
+
+  if( type != JOB_SYSTEM ) {
+    FXint max = ( type == JOB_RESERVED ? ID_RESERVED_MAX : ID_NORMAL_MAX );
+    FXint min = ( type == JOB_RESERVED ? 1 : ID_RESERVED_MAX + 1 );
+    FXbool found = false;
+
+    max++;
+    DEBUG_OUT( "[ProcSession::set_job] MIN = " << min << " MAX = " << max )
+    for( FXint i = min; i != max; i++ ) {
+      if( m_jobs[ i ] == 0 ) { result = i; }
+    }
+  }
+  else { result = 0; }
+
+  if( result >= 0 ) {
+    FXSystem::setEnvironment( m_name, FXString::value( result ) );
+    if( result > 0 ) { m_jobs[ result ] = 1; }
+    m_actjob = result;
+  }
+
+  return -1;
+}
+
+FXint ProcSession::check( )
+{
+  
+  FXint pid_value;
+  FXString checker = "/home/gabriel/Projects/Fox/sources/Gorgona/tmp/utils/grpchecker.sh";
+  
+  boost::asio::io_service ios3;
+  std::future<std::string> fdata;
+  BP::child filesystems( checker.text( ), BP::std_in.close( ), BP::std_out > fdata, BP::std_err > BP::null, ios3 );
+  ios3.run( );
+
+  m_maintable.clear( );
+  std::istringstream _out;
+  _out.str( fdata.get( ) );
+  for( std::string line; std::getline( _out, line); ) {
+    m_maintable.push_back( std::stoi( line ) );    
+  }
+  
+  std::cout << "Gorgana Session processes: " << std::endl;
+  std::cout << "===========================" << std::endl;
+  for( int i = 0; i != m_maintable.size( ); i++ ) { std::cout << m_maintable[ i ] << std::endl; }   
+  std::cout << std::endl;
+    
+  return (FXint) m_maintable.size( );
+}
+
+/*** END ******************************************************************************************/
+} /* PERSEUS */
