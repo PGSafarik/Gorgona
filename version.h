0ca3133d8a5ac0f04d1eacec78b1285fb0bd4d05
D.A.Tiger <drakarax@seznam.cz>
2025-11-05 13:27:02 +0100

    Changed of the project version

--- version.h
+++ version.h
@@ -3,7 +3,7 @@
 
 namespace AutoVersion{
 	//Date Version Types
-	static const char DATE[]                 = "04";
+	static const char DATE[]                 = "05";
 	static const char MONTH[]                = "11";
 	static const char YEAR[]                 = "2025";
 	static const char UBUNTU_VERSION_STYLE[] = "25.11";
@@ -16,14 +16,14 @@ namespace AutoVersion{
 	static const long MAJOR    = 1;
 	static const long MINOR    = 6;
 	static const long BUILD    = 7;
-	static const long REVISION = 0;
+	static const long REVISION = 1;
 	
 	//Miscellaneous Version Types
-	#define RC_FILEVERSION 1,6,7,0
-	#define RC_FILEVERSION_STRING "1, 6, 7, 0\0"
-	static const char FULLVERSION_STRING [] = "1.6.7.0";
+	#define RC_FILEVERSION 1,6,7,1
+	#define RC_FILEVERSION_STRING "1, 6, 7, 1\0"
+	static const char FULLVERSION_STRING [] = "1.6.7.1";
 	
-	static const long BUILDS_COUNT  = 1;
+	static const long BUILDS_COUNT  = 2;
 	static const long BUILD_HISTORY = 0;
 }
 #endif //VERSION_H
